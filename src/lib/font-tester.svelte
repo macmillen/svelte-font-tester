0d5198fb7a2a1340f0809ed31f3dd4ec83b7b0a3
Milan Jaritz <milan.jaritz@gmail.com>
2023-11-17 18:52:52 +0100

    remove tailwind

--- src/lib/font-tester.svelte
+++ src/lib/font-tester.svelte
@@ -27,10 +27,10 @@
   {/if}
 </svelte:head>
 
-<div class="fixed bottom-3 z-50 w-full flex justify-center">
-  <div class="rounded-xl h-full space-y-2 p-3 w-full max-w-[20rem] bg-black">
-    <p class="text-white font-bold">Font Tester</p>
-    <select bind:value={font} class="h-14 w-full rounded-xl px-4">
+<div class="__ft-container">
+  <div class="__ft-box">
+    <p class="__ft-header">Font Tester</p>
+    <select class="__ft-select" bind:value={font}>
       <option value="">Choose font</option>
       {#each fontOptions as item}
         <option>{item}</option>
@@ -38,3 +38,39 @@
     </select>
   </div>
 </div>
+
+<style>
+  .__ft-container {
+    position: fixed;
+    bottom: 0.75rem;
+    z-index: 50;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .__ft-box {
+    border-radius: 0.75rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 0.75rem;
+    width: 100%;
+    max-width: 20rem;
+    background-color: black;
+  }
+
+  .__ft-header {
+    background-color: white;
+    font-weight: 700;
+  }
+
+  .__ft-select {
+    height: 3.5rem;
+    width: 100%;
+    border-radius: 0.75rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+</style>
